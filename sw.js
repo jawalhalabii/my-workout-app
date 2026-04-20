a16e3b444ce4c5f6ef6ea70720fd1b2cbc9fdfd8
jawalhalabii <jawalhalabii@icloud.com>
2026-04-20 11:35:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,6 @@
-const CACHE = 'workout-app-v3';
-const FILES = ['/my-workout-app/', '/my-workout-app/index.html', '/my-workout-app/manifest.json', '/my-workout-app/icon.png', '/my-workout-app/icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {})));
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/my-workout-app/index.html'))));
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
